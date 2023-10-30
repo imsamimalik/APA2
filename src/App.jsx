03132207786fe652254c5aae70d92b291d5992c8
Akhtar Nadeem <imsamimalikk@gmail.com>
2023-10-30 18:03:33 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import Q1 from "@/Questions/Q1"
-import Q2 from "./Questions/Q2"
+import Q1 from "@/Questions/Q1";
+import Q2 from "./Questions/Q2";
 
 function App() {
-
   return (
     <>
       <Q1 />
       {/* <Q2 /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
